a6558cfedc8bda4d8f035b1d5cbc1ce5ad54cc18
ThatOneKid0203 <31484977+ThatOneKid0203@users.noreply.github.com>
2017-08-30 13:25:07 -0400

    Add files via upload

---
+++ M1T1_McKnight.py
@@ -0,0 +1,10 @@
+# M1T1
+# Hello World
+# Michael McKnight
+# 8/23/17
+
+# This program prints the phrase "Hello World" and adds numbers.
+
+print ('Hello World')
+print (19/6)
+print('Michael McKnight')
