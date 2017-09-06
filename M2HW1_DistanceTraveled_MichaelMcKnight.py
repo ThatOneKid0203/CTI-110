a8de5078c31540eb7e627188912ede81f6f6e6e8
ThatOneKid0203 <31484977+ThatOneKid0203@users.noreply.github.com>
2017-09-06 13:36:19 -0400

    Add files via upload

---
+++ M2HW1_DistanceTraveled_MichaelMcKnight.py
@@ -0,0 +1,15 @@
+# CTI-110 
+# M2HW1 - Distance Traveled  
+# Michael McKnight
+# 9/6/17
+
+# This program calculates the distance a vehicle travels at a certain speed, written as the distance equals speed * time.
+
+# First, 6 hours at 70 mph.
+print ("70 mph over 6 hours is equal to ", (70 * 6), "miles.")
+
+# Next, 10 hours.
+print ("70 mph over 10 hours is equal to ", (70 * 10), "miles.")
+
+# Finally 15 hours.
+print ("70 mph over 15 hours is equal to ", (70 * 15), "miles.")
